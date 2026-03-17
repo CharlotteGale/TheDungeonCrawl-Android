f4fa091f8c096b63fb2c91c6b83dc0b89bceb4a3
Charlotte Gale <CharlotteMGale@outlook.com>
2026-03-17 10:15:04 +0000

    adds room description func w conditionals for expanded descriptions

--- app/src/main/java/com/example/thedungeoncrawl/engine/GameEngine.kt
+++ app/src/main/java/com/example/thedungeoncrawl/engine/GameEngine.kt
@@ -23,7 +23,7 @@ class GameEngine {
             chests = mutableListOf(
                 Chest(
                     id = "wooden_chest",
-                    name = "Ornate Chest",
+                    name = "Wooden Chest",
                     isLocked = false,
                     isOpen = false,
                     contents = mutableListOf(
@@ -202,7 +202,7 @@ class GameEngine {
         val exit = currentRoom().exits[direction]
         return if (exit != null) {
             player.currentRoomId = exit
-            "You head in $direction"
+            "You head $direction"
         } else {
             "You can't go that way."
         }
@@ -401,6 +401,30 @@ class GameEngine {
                 "You can't use that key here."
             }
             else -> "You're not sure how to use that."
+        }
+    }
+
+    fun getRoomDescription(): String {
+        val room = currentRoom()
+        val base = room.description
+
+        return when (room.id) {
+            "altar_room" -> if (player.hasItem("lit_torch"))
+                "$base\n\nThe torchlight catches something in the shadows." +
+                        "\nA small key glints on the stone floor beside the altar."
+            else base
+            "barracks" -> {
+                val chestInfo = room.chests.joinToString("\n") { chest ->
+                    val status = when {
+                        chest.isLocked -> "locked"
+                        chest.isOpen -> "open"
+                        else -> "unlocked"
+                    }
+                    "${chest.name} ($status)"
+                }
+                "$base\n\n$chestInfo"
+            }
+            else -> base
         }
     }
 
